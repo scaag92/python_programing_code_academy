d4bef1803c2cc3297c8a18c7fe6d143de63c8f5a
Cristian Arias <cristianarias@Cristians-MacBook-Pro.local>
2019-04-07 22:16:31 -0500

    First lines

---
+++ Programing instead.py
@@ -0,0 +1,11 @@
+Programing instead
+
+1st Lesson:
+my_name = "Cristian"
+print("Hello and welcome " + my_name + "!")
+
+
+2nd Lesson., How to make comments:
+# Buena los perros
+persnickety_count = 0
+
