bb3a8517239a95514cd7dbead8fe560ce9393276
Cristian Arias <cristianarias@Cristians-MacBook-Pro.local>
2019-04-08 00:30:40 -0500

    kjaskjd

--- programing_instead.py
+++ programing_instead.py
@@ -4,8 +4,53 @@ Programing instead
 my_name = "Cristian"
 print("Hello and welcome " + my_name + "!")
 
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 2nd Lesson., How to make comments:
 # Buena los perros
 persnickety_count = 0
 
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+3rd - 4th Lesson, Print
+
+print ("Cristian")
+
+print ('Cristian')
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+5th Lesson: Variables =
+
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "Caldo de Costilla"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = "Pollito asado"
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+
+# Printing out dinner
+print("Calentado Paisa:")
+print(meal)
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+6th Lesson: Int & Float Variables 
+
+# Define the release and runtime integer variables below:
+
+release_year_int = 1998
+runtime_int = 192
+
+# Define the rating_out_of_10 float variable below: 
+
+rating_out_of_10_float = 1.55
