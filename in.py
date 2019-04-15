31b7c18f6ec143d9914f818370d3659a8e68ccef
Cristian Arturo Arias <cristian.arias@CO-IT02500.local>
2019-04-15 18:11:16 -0500

    test

---
+++ in.py
@@ -0,0 +1,6 @@
+print("1st Number:")
+num1 = input()
+print("2nd Number:")
+num2 = input()
+print(num1 + num2)
+
--- programing_instead.py
+++ programing_instead.py
@@ -423,5 +423,172 @@ def get_work(mass, acceleration, distance):
 train_work = get_work(train_mass, train_acceleration, train_distance)
 print("The GE train does" + train_work + "Joules of work over" + train_distance + "meters.".)
 
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Unit 2 - CODE CHALLENGE: PYTHON FUNCTIONS
+
+AVERAGE
+
+# Write your average function here:
+
+# Uncomment these function calls to test your average function:
+#print(average(1, 100))
+# The average of 1 and 100 is 50.5
+#print(average(1, -1))
+# The average of 1 and -1 is 0
+def average(num1, num2):
+  num1 += num2
+  result = num1 / 2
+  return result
+
+TENTH POWER
+
+# Write your tenth_power function here:
+
+# Uncomment these function calls to test your tenth_power function:
+#print(tenth_power(1))
+# 1 to the 10th power is 1
+#print(tenth_power(0))
+# 0 to the 10th power is 0
+#print(tenth_power(2))
+# 2 to the 10th power is 1024
+def tenth_power(num):
+  return num ** 10
+
+BOND
+
+# Write your introduction function here:
+
+# Uncomment these function calls to test your introduction function:
+#print(introduction("James", "Bond"))
+# should print Bond, James Bond
+#print(introduction("Maya", "Angelou"))
+# should print Angelou, Maya Angelou
+def introduction(first_name, last_name):
+  	return last_name + "," + " " +  first_name + " " + last_name
+  
+SQUARE ROOT
+
+# Write your square_root function here:
+
+# Uncomment these function calls to test your square_root function:
+#print(square_root(16))
+# should print 4
+#print(square_root(100))
+# should print 10
+def square_root(num):
+  return num**(1/2)
+
+num1 = square_root(8)
+print(num1)
+
+TIP
+
+# Write your tip function here:
+  
+# Uncomment these function calls to test your tip function:
+#print(tip(10, 25))
+# should print 2.5
+#print(tip(0, 100))
+# should print 0.0
+
+def tip(total, percentage):
+  tip = (( total * percentage ) / 100 )
+  return tip
+  
+WIN PERCENTAGE
+
+# Write your win_percentage function here:
+
+# Uncomment these function calls to test your win_percentage function:
+#print(win_percentage(5, 5))
+# should print 50
+#print(win_percentage(10, 0))
+# should print 100
+
+def win_percentage(wins, losses):
+  total = (wins + losses)
+  wins_percentage = wins * 100 / total
+  return wins_percentage
+
+
+FIRST THREE MULTIPLES
+
+# Write your first_three_multiples function here:
+def first_three_multiples(num):
+  print(num)
+  print(num*2)
+  print(num*3)
+  return num*3
+
+# Uncomment these function calls to test your first_three_multiples function:
+first_three_multiples(10)
+# should print 10, 20, 30, and return 30
+first_three_multiples(0)
+# should print 0, 0, 0, and return 0
+
+DOG YEARS
+
+# Write your dog_years function here:
+def dog_years(name, age):
+  age = age * 7
+  return name + ", you are " + str(age) + " years old in dog years"
+# Uncomment these function calls to test your dog_years function:
+print(dog_years("Lola", 16))
+# should print "Lola, you are 112 years old in dog years"
+print(dog_years("Baby", 0))
+# should print "Baby, you are 0 years old in dog years"
+
+
+REMAINDER
+
+# Write your remainder function here:
+def remainder(num1, num2):
+  return (2*num1)%(num2/2)
+
+# Uncomment these function calls to test your remainder function:
+print(remainder(15, 14))
+# should print 2
+print(remainder(9, 6))
+# should print 0
+
+LOTS OF MATH
+
+# Write your lots_of_math function here:
+def lots_of_math(a, b, c, d):
+  print(a + b)
+  print(d - c)
+  print((a + b)*(d - c))
+  return (((a + b)*(d - c))%a)
+  
+# Uncomment these function calls to test your lots_of_math function:
+print(lots_of_math(1, 2, 3, 4))
+# should print 3, -1, -3, 0
+#print(lots_of_math(1, 1, 1, 1))
+# should print 2, 0, 0, 0
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Unit 3 - CODE CHALLENGE: PYTHON FUNCTIONS
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Unit 3rd - Lesson 2nd - Boolean Exzpressions
+
+# My dog is the cutest dog in the world.
+example_statement = "No"
+
+# Dogs are mammals.
+statement_one = "Yes"
+
+# My dog is named Pavel.
+statement_two = "Yes"
+
+# Dogs make the best pets.
+statement_three = "No"
+
+# Cats are female dogs.
+statement_four = "Yes"
+
 
 
