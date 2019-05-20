2e78dba8ac04065fe3ef0ba0fb5379a4ecab7faf
Cristian Arias <scaag92@gmail.com>
2019-05-20 17:00:07 -0500

    Update programing_instead.py
    Unit 6th - Lesson 12th - CARLYS CLIPPERS

--- programing_instead.py
+++ programing_instead.py
@@ -1553,5 +1553,52 @@ for digit in single_digits:
 
 print(cubes)
 
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Unit 6th - Lesson 12th - CARLYS CLIPPERS
+
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+total_price = 0
+
+for pr in prices:
+  total_price += pr
+
+average_price = total_price / len(prices) 
+
+print("Average Haircut Price: " + str(average_price))
+
+new_prices=[]
+
+for pr in prices:
+  number = pr -5
+  new_prices.append(number)
+
+print(new_prices)
+
+total_revenue = 0
+
+for i in range(len(hairstyles)): 
+  total_revenue += prices[i] * last_week[i]
+
+print("Total Revenue: " + str(total_revenue))
+average_daily_revenue = total_revenue / 7 
+print("Average daily revenue " + str(average_daily_revenue))
+
+cuts_under_30=[]
+
+for i in new_prices:
+  if i > 30:
+    cuts_under_30=[hairstyles[i] , new_prices[i]]
+
+print(cuts_under_30)
+
+
+
+
 
 
